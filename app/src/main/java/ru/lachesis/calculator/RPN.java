accfc9ca6011d7c18ca1622f559d6ab539d0d0c9
lachesis-lab <mgmmail@mail.ru>
2021-03-04 16:25:47 +0300

    Добавлен лендскейпный лэйаут, класс оболочка RPNWrapper, добавлено удаление последнего ввода и всего ввода полностью (по долгому нажатию "C")

--- app/src/main/java/ru/lachesis/calculator/RPN.java
+++ app/src/main/java/ru/lachesis/calculator/RPN.java
@@ -1,57 +1,17 @@
 package ru.lachesis.calculator;
 
 import android.content.Context;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RPN implements Parcelable {
+public class RPN {
 
-    private List<String> outputList = new ArrayList<>();
-    private List<String> operationStack = new ArrayList<>();
-    private StringBuilder currentNumber = new StringBuilder();
+    private List<String> mOutputList = new ArrayList<>();
+    private List<String> mOperationStack = new ArrayList<>();
     private String mOperationPlus, mOperationMinus, mOperationMultiply, mOperationDivide, mOperationNegative;
 
-    protected RPN(Parcel in) {
-        outputList = in.createStringArrayList();
-        operationStack = in.createStringArrayList();
-        mOperationPlus = in.readString();
-        mOperationMinus = in.readString();
-        mOperationMultiply = in.readString();
-        mOperationDivide = in.readString();
-        mOperationNegative = in.readString();
-    }
-
-    public static final Creator<RPN> CREATOR = new Creator<RPN>() {
-        @Override
-        public RPN createFromParcel(Parcel in) {
-            return new RPN(in);
-        }
-
-        @Override
-        public RPN[] newArray(int size) {
-            return new RPN[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeStringList(outputList);
-        dest.writeStringList(operationStack);
-        dest.writeString(mOperationPlus);
-        dest.writeString(mOperationMinus);
-        dest.writeString(mOperationMultiply);
-        dest.writeString(mOperationDivide);
-        dest.writeString(mOperationNegative);
-    }
 
     private static class Top {
         private int mIndex;
@@ -71,45 +31,56 @@ public class RPN implements Parcelable {
         mOperationNegative = context.getString(R.string.buttonTextPlusMinus);
     }
 
-    public List<String> makeRPN(String symbol) {
+    public List<String> makeRPN(List<String> inputList) {
+        List<String> result = new ArrayList<>();
+        for (String symbol : inputList) {
+            result = addSymbol(symbol);
+        }
+        return result;
+    }
 
+    private List<String> addSymbol(String symbol) {
         Top top = getStackTop();
-        if (isDigit(symbol) || isPoint(symbol)) {
-            currentNumber.append(symbol);
-        } else {
-            if (!"(".contains(symbol) && currentNumber.length() != 0)
-                outputList.add(currentNumber.toString());
+//        if (isDigit(symbol) || isPoint(symbol)) {
+//            mCurrentNumber.append(symbol);
+//        } else {
+//            if (!"(".contains(symbol) && mCurrentNumber.length() != 0)
+//                mOutputList.add(mCurrentNumber.toString());
+        try {
+            Double.parseDouble(symbol);
+            mOutputList.add(symbol);
+        } catch (Exception e) {
             if (getPriority(symbol) > 0 || "=".equals(symbol)) {
                 //операции
                 while (top != null && getPriority(symbol) <= getPriority(top.mValue)) {
                     if (isBrackets(top.mValue))
                         break;
-                    outputList.add(top.mValue);
+                    mOutputList.add(top.mValue);
                     top = removeStackTop(top);
                 }
                 top = addStackTop(top, symbol);
             } else if (symbol.equals(")")) {
-                while (top != null) { //&& !top.mValue.contains("(")) {
+                while (top != null) {
                     if (top.mValue.equals("(")) {
                         top = removeStackTop(top);
                         break;
                     } else {
-                        outputList.add(top.mValue);
+                        mOutputList.add(top.mValue);
                         top = removeStackTop(top);
                     }
                 }
             } else if (top != null)
                 addStackTop(top, symbol);
-            currentNumber.setLength(0);
+//            mCurrentNumber.setLength(0);
         }
-        return outputList;
+        return mOutputList;
     }
 
     private Top removeStackTop(Top top) {
         try {
-            operationStack.remove(operationStack.size() - 1);
-            top.mIndex = operationStack.size() - 1;
-            top.mValue = operationStack.get(operationStack.size() - 1);
+            mOperationStack.remove(mOperationStack.size() - 1);
+            top.mIndex = mOperationStack.size() - 1;
+            top.mValue = mOperationStack.get(mOperationStack.size() - 1);
             if (top.mIndex < 0) return null;
             else return top;
         } catch (Exception e) {
@@ -119,9 +90,9 @@ public class RPN implements Parcelable {
 
     private Top addStackTop(Top top, String symbol) {
         try {
-            operationStack.add(symbol);
-            top.mIndex = operationStack.size() - 1;
-            top.mValue = operationStack.get(operationStack.size() - 1);
+            mOperationStack.add(symbol);
+            top.mIndex = mOperationStack.size() - 1;
+            top.mValue = mOperationStack.get(mOperationStack.size() - 1);
             return top;
         } catch (Exception e) {
             return null;
@@ -129,25 +100,27 @@ public class RPN implements Parcelable {
     }
 
     private Top getStackTop() {
-        if (operationStack.size() == 0) return null;
+        if (mOperationStack.size() == 0) return null;
         else {
-            return new Top(operationStack.size() - 1, operationStack.get(operationStack.size() - 1));
+            return new Top(mOperationStack.size() - 1, mOperationStack.get(mOperationStack.size() - 1));
         }
     }
 
+/*
     public void clearStacks() {
-        operationStack.clear();
-        outputList.clear();
-        currentNumber.setLength(0);
+        mOperationStack.clear();
+        mOutputList.clear();
+//        mCurrentNumber.setLength(0);
     }
-
+*/
+/*
     private boolean isDigit(String symbol) {
         return "0123456789".contains(symbol);
     }
 
     private boolean isPoint(String symbol) {
         return ".".equals(symbol);
-    }
+    }*/
 
     private boolean isBrackets(String symbol) {
         return "()".contains(symbol);
@@ -163,33 +136,43 @@ public class RPN implements Parcelable {
         else return 0;
     }
 
-    public Double calculateRPN() {
+    public Double calculateRPN() throws RuntimeException {
         Double result = 0D;
+        boolean isMyException = false;
         List<Double> stack = new ArrayList<>();
-        for (int i = 0; i < outputList.size(); i++) {
-            String value = outputList.get(i);
+        for (int i = 0; i < mOutputList.size(); i++) {
+            String value = mOutputList.get(i);
             try {
                 stack.add(Double.parseDouble(value));
             } catch (Exception e) {
-                if (value.equals(mOperationPlus)) {
-                    result = stack.get(stack.size() - 2) + stack.get(stack.size() - 1);
-                    afterBinaryOperation(stack, result);
-                } else if (value.equals(mOperationMinus)) {
-                    result = stack.get(stack.size() - 2) - stack.get(stack.size() - 1);
-                    afterBinaryOperation(stack, result);
-                } else if (value.equals(mOperationMultiply)) {
-                    result = stack.get(stack.size() - 2) * stack.get(stack.size() - 1);
-                    afterBinaryOperation(stack, result);
-                } else if (value.equals(mOperationDivide)) {
-                    result = stack.get(stack.size() - 2) / stack.get(stack.size() - 1);
-                    afterBinaryOperation(stack, result);
-                } else if (value.equals(mOperationNegative)) {
-                    result = -1 * stack.get(stack.size() - 1);
-                    afterUnaryOperation(stack, result);
+                try {
+                    if (value.equals(mOperationPlus)) {
+                        result = stack.get(stack.size() - 2) + stack.get(stack.size() - 1);
+                        afterBinaryOperation(stack, result);
+                    } else if (value.equals(mOperationMinus)) {
+                        result = stack.get(stack.size() - 2) - stack.get(stack.size() - 1);
+                        afterBinaryOperation(stack, result);
+                    } else if (value.equals(mOperationMultiply)) {
+                        result = stack.get(stack.size() - 2) * stack.get(stack.size() - 1);
+                        afterBinaryOperation(stack, result);
+                    } else if (value.equals(mOperationDivide)) {
+                        if (stack.get(stack.size() - 1) == 0) {
+                            isMyException = true;
+                            throw new RuntimeException("Div0");
+                        }
+                        result = stack.get(stack.size() - 2) / stack.get(stack.size() - 1);
+                        afterBinaryOperation(stack, result);
+                    } else if (value.equals(mOperationNegative)) {
+                        result = -1 * stack.get(stack.size() - 1);
+                        afterUnaryOperation(stack, result);
+                    }
+                } catch (RuntimeException ie) {
+                    throw new RuntimeException(!isMyException ? "WrongInput" : ie.getMessage());
                 }
             }
         }
         Log.i("RESULT", result.toString());
+        if (Double.isInfinite(result)) throw new ArithmeticException("Infinite");
         return result;
     }
 
---
+++ app/src/main/java/ru/lachesis/calculator/RPNWrapper.java
@@ -0,0 +1,70 @@
+package ru.lachesis.calculator;
+
+import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RPNWrapper implements Parcelable {
+    List<String> mInputList = new ArrayList<>();
+    //    List<String> mOutputList = new ArrayList<>();
+    Context mContext;
+    RPN rpn;
+
+    public RPNWrapper(Context context) {
+        mContext = context;
+    }
+
+    public List<String> getInputList() {
+        return mInputList;
+    }
+
+    public void setInputList(List<String> inputList) {
+        mInputList = inputList;
+    }
+
+    protected RPNWrapper(Parcel in) {
+        mInputList = in.createStringArrayList();
+    }
+
+    public static final Creator<RPNWrapper> CREATOR = new Creator<RPNWrapper>() {
+        @Override
+        public RPNWrapper createFromParcel(Parcel in) {
+            return new RPNWrapper(in);
+        }
+
+        @Override
+        public RPNWrapper[] newArray(int size) {
+            return new RPNWrapper[size];
+        }
+    };
+
+    public List<String> buildRPNString(List<String> inputList) {
+        rpn = new RPN(mContext);
+        mInputList = inputList;
+        return rpn.makeRPN(mInputList);
+    }
+
+
+    public Double calculate() {
+        if (rpn == null) return 0D;
+        else
+            return rpn.calculateRPN();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringList(mInputList);
+    }
+
+    public void clearStacks() {
+        mInputList.clear();
+    }
+}
